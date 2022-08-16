896d8230f8df47a8ec0b12417f4dfedc1dc69367
Steve Ou <300steveou@gmail.com>
2022-08-16 16:07:56 +0800

    let 更正

--- all.js
+++ all.js
@@ -1,11 +1,10 @@
-
 // hositing
 // 全域變數 _ 底線
 
 init();
 
-var countyData = [];
-var mapData= [];
+var _countyData = [];
+var _mapData= [];
 
 function init(){ 
    getCountyData();
@@ -15,13 +14,13 @@ function init(){
 
 function renderDay(){
   
-   var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-   var current_datetime = new Date();
-   var currentDay = current_datetime.getDay();
-   var dayName = dayArray[currentDay]; 
+   let dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+   let current_datetime = new Date();
+   let currentDay = current_datetime.getDay();
+   let dayName = dayArray[currentDay]; 
    document.querySelector('.jsDate').textContent = dayName;
    
-   var currentDate = current_datetime.getFullYear()+"/"+(current_datetime.getMonth()+1)+"/"+current_datetime.getDate();
+   let currentDate = current_datetime.getFullYear()+"/"+(current_datetime.getMonth()+1)+"/"+current_datetime.getDate();
    console.log(currentDate,'currentDate'); 
    
    judgeEvenOdd(currentDay);
@@ -38,14 +37,14 @@ function judgeEvenOdd(currentDay){
 
 // 取得地圖資料
 function getMapData(){
-   var xhr = new XMLHttpRequest();
-   var apiUrl = 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR0oowBRjj1goAMqtnugBiXMTMY8OCl14TGmgt3YDJi9w5BXs4VsfZQ9mDI';
+   let xhr = new XMLHttpRequest();
+   let apiUrl = 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR0oowBRjj1goAMqtnugBiXMTMY8OCl14TGmgt3YDJi9w5BXs4VsfZQ9mDI';
 
    xhr.open('get',apiUrl);
    xhr.send();
    xhr.onload = function(){
       //  console.log(xhr.responseText);
-      mapData = JSON.parse(xhr.responseText);
+      _mapData = JSON.parse(xhr.responseText);
       var defaultCounty ='臺北市';
       renderLeftNodeList(defaultCounty);
    }
@@ -53,23 +52,23 @@ function getMapData(){
 
 // 取得縣市名稱
 function getCountyData(){
-   var xhr = new XMLHttpRequest();
-   var apiUrl = 'https://demeter.5fpro.com/tw/zipcode/cities.json';
+   let xhr = new XMLHttpRequest();
+   let apiUrl = 'https://demeter.5fpro.com/tw/zipcode/cities.json';
 
    xhr.open('get',apiUrl);
    xhr.send();
    xhr.onload = function(){
        
-      countyData = JSON.parse(xhr.responseText);       
+      _countyData = JSON.parse(xhr.responseText);       
       renderCountySelectItems();
    }
 }
 
 function renderCountySelectItems(){
-   var array = countyData;   
-    var sel = document.querySelector('.area');
+   let array = _countyData;   
+   let sel = document.querySelector('.area');
 
-    for (var i=0;i<array.length;i++) {
+    for (let i=0;i<array.length;i++) {
       let opt = document.createElement('option');
       opt.value = array[i].name;
       opt.text = array[i].name;
@@ -79,22 +78,19 @@ function renderCountySelectItems(){
 }
 
 function renderLeftNodeList(defaultCounty){
-   var array = mapData.features;
-   console.log(mapData);
-    var str= "";
+   let array = _mapData.features;
+   console.log(_mapData);
+   let str= "";
 
-   for(var i =0;i<array.length;i++){
+   for(let i =0;i<array.length;i++){
 
       if(array[i].properties.county===defaultCounty){
          str+='<li>'+array[i].properties.name+ '</li>' +array[i].properties.mask_child ;
       }      
    }
-  console.log(str);
    document.querySelector('.list').innerHTML = str;
 }
 
-document.querySelector('.area').addEventListener('change',function(e){
-   console.log(e.target.value);
+document.querySelector('.area').addEventListener('change',function(e){   
    renderLeftNodeList(e.target.value);
 });
- 
